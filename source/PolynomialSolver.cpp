176da0e9d524ef9c52f8e2804aa92790c145bce1
Bryce Mehring <bm4g5@mst.edu>
2014-02-21 03:23:23 -0600

    Added Polynomial solver

---
+++ source/PolynomialSolver.cpp
@@ -0,0 +1,85 @@
+#include "PolynomialSolver.h"
+#include <gsl/gsl_poly.h>
+#include <gsl/gsl_errno.h>
+
+struct Polynomial::PIMPL
+{
+	gsl_poly_complex_workspace* w = nullptr;
+};
+
+Polynomial::Polynomial()
+{
+	m_pGSLWorkspace = new Polynomial::PIMPL();
+}
+
+Polynomial::Polynomial(Polynomial &&other) : m_pGSLWorkspace(nullptr)
+{
+	*this = std::move(other);
+}
+
+Polynomial& Polynomial::operator =(Polynomial&& other)
+{
+	delete m_pGSLWorkspace;
+	m_pGSLWorkspace = other.m_pGSLWorkspace;
+	other.m_pGSLWorkspace = nullptr;
+
+	m_coeffecients = std::move(other.m_coeffecients);
+	m_results = std::move(other.m_results);
+	m_fEqual = other.m_fEqual;
+	m_iPower = other.m_iPower;
+
+	return *this;
+}
+
+Polynomial::~Polynomial()
+{
+	if(m_pGSLWorkspace != nullptr)
+	{
+		gsl_poly_complex_workspace_free(m_pGSLWorkspace->w);
+		delete m_pGSLWorkspace;
+	}
+}
+
+void Polynomial::SetEquation(double eq, unsigned int power)
+{
+	if((power + 1) != m_coeffecients.size())
+	{
+		gsl_poly_complex_workspace_free(m_pGSLWorkspace->w);
+		m_pGSLWorkspace->w = nullptr;
+	}
+
+	m_fEqual = eq;
+	m_iPower = power;
+
+	m_coeffecients.resize(m_iPower + 1,1.0);
+	m_results.resize(2*m_iPower);
+	m_coeffecients[0] = -eq;
+
+	if(m_pGSLWorkspace->w == nullptr)
+	{
+		m_pGSLWorkspace->w = gsl_poly_complex_workspace_alloc(m_coeffecients.size());
+
+		if(m_pGSLWorkspace->w == nullptr)
+		{
+			throw std::bad_alloc();
+		}
+	}
+}
+
+double Polynomial::GetRoot(unsigned int index) const
+{
+	if(m_results.empty() || index >= m_results.size())
+		return 0.0;
+
+	return m_results[index];
+}
+
+bool Polynomial::Solve()
+{
+	if(m_coeffecients.empty())
+		return false;
+
+	int success = gsl_poly_complex_solve(m_coeffecients.data(), m_coeffecients.size(), m_pGSLWorkspace->w, m_results.data());
+
+	return success == GSL_SUCCESS;
+}
---
+++ source/PolynomialSolver.h
@@ -0,0 +1,39 @@
+#ifndef _POLYNOMIALSOLVER_
+#define _POLYNOMIALSOLVER_
+#include <vector>
+
+class Polynomial
+{
+public:
+
+	Polynomial();
+	~Polynomial();
+
+	Polynomial(const Polynomial&) = delete;
+	Polynomial(Polynomial&& other);
+
+	Polynomial& operator=(Polynomial&& other);
+
+	// Sets the following equation form:
+	// A = x + x^2 + x^3 + ... + x^B
+	void SetEquation(double A, unsigned int B);
+
+	// Returns the root specified by index
+	double GetRoot(unsigned int index = 0) const;
+
+	// Finds all the roots for the equation set
+	// Returns true if the equation is solvable, false otherwise
+	bool Solve();
+
+private:
+
+	struct PIMPL;
+	PIMPL* m_pGSLWorkspace;
+
+	std::vector<double> m_results;
+	std::vector<double> m_coeffecients;
+	double m_fEqual = 0.0;
+	unsigned int m_iPower = 0;
+};
+
+#endif // _POLYNOMIALSOLVER_
